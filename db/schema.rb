fa8d821a8e648e0fd9572f8df86835e98b23572f
joti sempe <josefinasempe@gmail.com>
2018-06-13 12:56:52 -0300

    changed raiting for rating name (typoerror). migrated again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_153528) do
+ActiveRecord::Schema.define(version: 2018_06_13_155247) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_06_13_153528) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
-    t.integer "raiting"
+    t.integer "rating"
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
